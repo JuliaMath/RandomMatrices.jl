0f6d627f69ffd7d0d40c80f65178743278157c05
Jiahao Chen <jiahao@mit.edu>
2013-03-20 20:44:14 -0400

    Sparse implementation of FormalPowerSeries
    It still bugs out but I can't really figure out why.

--- src/FormalPowerSeries.jl
+++ src/FormalPowerSeries.jl
@@ -11,87 +11,126 @@
 
 import Base.inv, Base.length
 
+#############################
+# Formal power series (fps) #
+#############################
+
 # Definition [H, p.9] - we limit this to finitely many coefficients
 type FormalPowerSeries{Coefficients}
-    #TODO this really should be a sparse vector
-    c :: Vector{Coefficients}
-    FormalPowerSeries{Coefficients <: Number}(c::Vector{Coefficients}) = new(c)
+    c :: Dict{BigInt, Coefficients}
+    FormalPowerSeries{Coefficients}(c::Dict{BigInt, Coefficients}) = new(c)
+    function FormalPowerSeries{Coefficients}(v::Vector{Coefficients})
+        c=Dict{BigInt, Coefficients}()
+        for i=1:length(v)
+            if v[i] != 0
+                c[i-1] = v[i] #Note this off by 1 - allows constant term c[0] to be set
+            end     
+        end
+        FormalPowerSeries{Coefficients}(c)
+    end
 end
 
-#Comparisons allowed between fps and Vector
+
+
+#Return truncated vector with c[i+1] = P[i]
+function tovector{T}(P::FormalPowerSeries{T}, n :: Integer)
+    c = zeros(n)
+    for (k,v) in P.c
+        if 1<=k+1<=n
+            c[k+1]=v
+        end
+    end
+    c
+end
+
+
 function =={T}(P::FormalPowerSeries{T}, Q::FormalPowerSeries{T})
-    a, b = P.c, Q.c
-    n1, n2 = length(a), length(b)
-    #Pad shorter coefficient list with zeros
-    (n1 > n2) ? (b = [b; zeros(T, n1 - n2)]) : (a = [a; zeros(T, n2 - n1)])
-    return all([a[i] == b[i] for i=1:max(n1, n2)])
+    for (k,v) in P.c
+        if v==0 #ignore explicit zeros
+            continue
+        elseif !has(Q.c, k)
+            return false
+        elseif Q.c[k] != v
+            return false
+        end
+    end
+    for (k,v) in Q.c
+        if v==0 #ignore explicit zeros
+            continue
+        elseif !has(P.c, k)
+            return false
+        elseif P.c[k] != v
+            return false
+        end
+    end
+    return true
 end
 
-function =={T}(P::Vector{T}, Q::FormalPowerSeries{T})
-    a, b = P, Q.c
-    n1, n2 = length(a), length(b)
-    #Pad shorter coefficient list with zeros
-    (n1 > n2) ? (b = [b; zeros(T, n1 - n2)]) : (a = [a; zeros(T, n2 - n1)])
-    return all([a[i] == b[i] for i=1:max(n1, n2)])
-end
 
-function =={T}(P::FormalPowerSeries{T}, Q::Vector{T})
-    a, b = P.c, Q
-    n1, n2 = length(a), length(b)
-    #Pad shorter coefficient list with zeros
-    (n1 > n2) ? (b = [b; zeros(T, n1 - n2)]) : (a = [a; zeros(T, n2 - n1)])
-    return all([a[i] == b[i] for i=1:max(n1, n2)])
-end
 
 # Basic housekeeping and properties
 
 # Remove extraneous zeros
 function trim{T}(P::FormalPowerSeries{T})
-    NumExtraZeros::Integer = 0
-    for k=length(P.c):-1:2
-        P.c[k]==convert(T, 0) ? (NumExtraZeros+=1) : break
+    for (k,v) in P.c
+        if v==0
+            delete!(P.c, k)
+        end
     end
-    return FormalPowerSeries{T}(copy(P.c[1:end-NumExtraZeros]))
+    return P
 end
 
-length{T}(P::FormalPowerSeries{T})=length(P.c)
+length{T}(P::FormalPowerSeries{T})=max([k for (k,v) in P.c])
 
 # Basic arithmetic [H, p.10]
 function +{T}(P::FormalPowerSeries{T}, Q::FormalPowerSeries{T})
-    a, b = P.c, Q.c
-    n1, n2 = length(a), length(b)
-    #Pad shorter coefficient list with zeros
-    (n1 > n2) ? (b = [b; zeros(T, n1 - n2)]) : (a = [a; zeros(T, n2 - n1)])
-    FormalPowerSeries{T}(a + b)
+    c = Dict{BigInt, T}()
+    for (k,v) in P.c
+        has(c,k) ? (c[k]+=v) : (c[k]=v) 
+    end
+    for (k,v) in Q.c
+        has(c,k) ? (c[k]+=v) : (c[k]=v) 
+    end
+    FormalPowerSeries{T}(c)
 end
 
 function -{T}(P::FormalPowerSeries{T}, Q::FormalPowerSeries{T})
-    a, b = P.c, Q.c
-    n1, n2 = length(a), length(b)
-    #Pad shorter coefficient list with zeros
-    (n1 > n2) ? (b = [b; zeros(T, n1 - n2)]) : (a = [a; zeros(T, n2 - n1)])
-    FormalPowerSeries{T}(a - b)
+    c = Dict{BigInt, T}()
+    for (k,v) in P.c
+        has(c,k) ? (c[k]+=v) : (c[k]=v) 
+    end
+    for (k,v) in Q.c
+        has(c,k) ? (c[k]-=v) : (c[k]=-v) 
+    end
+    FormalPowerSeries{T}(c)
 end
 
 #negation
--{T}(P::FormalPowerSeries{T}) = FormalPowerSeries{T}(-P.c)
+function -{T}(P::FormalPowerSeries{T})
+    c = Dict{BigInt, T}()
+    for (k,v) in P.c
+        c[k] = -v
+    end
+    FormalPowerSeries{T}(c)
+end
 
 #multiplication by scalar
-*{T}(P::FormalPowerSeries{T}, k::Number) = FormalPowerSeries{T}(P.c * k)
-*{T}(k::Number, P::FormalPowerSeries{T}) = FormalPowerSeries{T}(P.c * k)
+function *{T}(P::FormalPowerSeries{T}, n::Number)
+    c = Dict{BigInt, T}()
+    for (k,v) in P.c
+        c[k] = n*v
+    end
+    FormalPowerSeries{T}(c)
+end
+*{T}(n::Number, P::FormalPowerSeries{T}) = *(P, n)
 
 #Cauchy product [H, p.10]
-#
-#TODO This is actually a discrete convolution so it should be amenable
-#      to an FFT algorithm
 *{T}(P::FormalPowerSeries{T}, Q::FormalPowerSeries{T}) = CauchyProduct(P, Q)
 function CauchyProduct{T}(P::FormalPowerSeries{T}, Q::FormalPowerSeries{T})
-    a, b = P.c, Q.c
-    n1, n2 = length(a), length(b)
-    c = zeros(T, n1+n2-1)
-    for i1=1:n1
-        for i2=1:n2
-            c[i1+i2-1] += a[i1]*b[i2]
+    c = Dict{BigInt, T}()
+    for (k1, v1) in P.c
+        for (k2, v2) in Q.c
+            has(c, k1+k2) ? (c[k1+k2]+=v1*v2) : (c[k1+k2]=v1*v2)
         end
     end
     FormalPowerSeries{T}(c)
@@ -100,21 +139,26 @@ end
 #Hadamard product [H, p.10] - the elementwise product
 .*{T}(P::FormalPowerSeries{T}, Q::FormalPowerSeries{T}) = HadamardProduct(P, Q)
 function HadamardProduct{T}(P::FormalPowerSeries{T}, Q::FormalPowerSeries{T})
-    a, b = P.c, Q.c
-    n1, n2 = length(a), length(b)
-    n1>n2 ? (a=a[1:n2]) : (b=b[1:n1])
-    FormalPowerSeries{T}(a.*b)
+    c = Dict{BigInt, T}()
+    for (k,v) in P.c
+        if v!=0 && has(Q.c,k) && Q.c[k]==0
+            c[k] = v * Q.c[k]
+        end
+    end
+    FormalPowerSeries{T}(c)
 end
 
 #The identity element over the complex numbers
 function eye{T <: Number}(P::FormalPowerSeries{T})
-    FormalPowerSeries{T}([convert(T, 0), convert(T, 1)])
+    c = Dict{BigInt, T}()
+    c[0] = 1
+    FormalPowerSeries{T}(c)
 end
 
 isunit{T <: Number}(P::FormalPowerSeries{T}) = P==eye(P)
 
 # [H, p.12]
-isnonunit{T}(P::FormalPowerSeries{T}) = P.c[1]==0 && !isunit(P)
+isnonunit{T}(P::FormalPowerSeries{T}) = (!has(P.c, 0) || P.c[0]==0) && !isunit(P)
 
 #Constructs the top left m x m block of the (infinite) semicirculant matrix
 #associated with the fps [H, Sec.1.3, p.14]
@@ -123,10 +167,15 @@ isnonunit{T}(P::FormalPowerSeries{T}) = P.c[1]==0 && !isunit(P)
 #This constructs the dense matrix - Toeplitz matrices don't exist in Julia yet
 function MatrixForm{T}(P::FormalPowerSeries{T}, m :: Integer)
     m<0 ? error(sprintf("Invalid matrix dimension %d requested", m)) : true
-    
-    a = P.c
-    n = length(a)
-    M = sum([diagm(repmat(ones(T,1,1)*a[i], 1, m+1-i), i-1) for i=1:min(m, n)])
+    M=zeros(T, m, m)
+    for (k,v) in P.c
+        if k < m
+            for i=1:m-k
+                M[i, i+k]=v
+            end
+        end
+    end
+    M
 end
 
 #Reciprocal of power series [H, Sec.1.3, p.15]
@@ -143,12 +192,16 @@ end
 #TODO implement the FFT-based algorithm of one of the following
 #  doi:10.1109/TAC.1984.1103499
 #  https://cs.uwaterloo.ca/~glabahn/Papers/tamir-george-1.pdf
-function inv{T}(P::FormalPowerSeries{T}, Size :: Integer)
-    Size<0 ? error(sprintf("Invalid inverse truncation length %d requested", Size)) : true
-    
-    a = Size>=length(P) ? [P.c; zeros(T, Size-length(P))] : P.c[1:Size]
-    n = length(a)
-    a[1] == 0 ? (error("Inverse does not exist")) : true
+function reciprocal{T}(P::FormalPowerSeries{T}, n :: Integer)
+    n<0 ? error(sprintf("Invalid inverse truncation length %d requested", n)) : true
+
+    a = zeros(n) #Extract original coefficients in vector
+    for (k,v) in P.c
+        if 1<=k+1<=n
+            a[k+1] = v
+        end
+    end
+    a[1]==0 ? (error("Inverse does not exist")) : true
     inv_a1 = T<:Number ? 1.0/a[1] : inv(a[1])
     
     b = zeros(n) #Inverse
@@ -162,17 +215,25 @@ end
 
 #Derivative of fps [H. Sec.1.4, p.18] 
 function derivative{T}(P::FormalPowerSeries{T})
-    a = P.c
-    n = length(a)
-    FormalPowerSeries{T}(n==1 ? [convert(T, 0)]: [(k-1)*a[k] for k=2:n])
-end
-
-function isconstant{T <: Number}(P::FormalPowerSeries{T})
-    length(P.c)==1 || all([c==0 for c in P.c[2:end]])
+    c = Dict{BigInt, T}()
+    for (k,v) in P.c
+        if k != 0 && v != 0 
+            c[k-1] = k*v
+        end
+    end
+    FormalPowerSeries{T}(c)
 end
 
 #[H, Sec.1.4, p.18]
-isconst{T}(P::FormalPowerSeries{T}) = length(P)==1 || all([x==0 for x in P[2:end]])
+function isconstant{T}(P::FormalPowerSeries{T})
+    for (k,v) in P.c
+        if k!=0 && v!=0
+            return false
+        end
+    end
+    return true
+end
+
 
 
 # Power of fps [H, Sec.1.5, p.35]
@@ -191,108 +252,103 @@ function ^{T}(P::FormalPowerSeries{T}, n :: Integer)
 end
 
 
-# Composition of fps [H, Sec.1.5, p.35]
-#TODO this doesn't work in infix notation yet
-#(⋅){T}(P::FormalPowerSeries{T}, Q::FormalPowerSeries{T}) = compose(P, Q)
-function compose{T}(P::FormalPowerSeries{T}, Q::FormalPowerSeries{T})
-    a, b = P.c, Q.c
-    n, m = length(a), length(b)
 
-    l = (m-1)*(n-1)+1
-    bb = zeros(T, n, l)
-    QQ = FormalPowerSeries{T}([convert(T, 1)])
-    for i=1:n
-        bb[i, 1:length(QQ.c)] = QQ.c
-        QQ *= Q
-    end
-    for i=1:n
-        bb[i, :] *= a[i]
-    end
-    
-    return FormalPowerSeries{T}(sum(bb, 1)'[:,1])
+# Composition of fps [H, Sec.1.5, p.35]
+# This is the quick and dirty version
+function compose{T}(P::FormalPowerSeries{T}, Q::FormalPowerSeries{T})
+    sum([v * Q^k for (k, v) in P.c]) 
 end
 
 
 #[H, p.45]
-isalmostunit{T}(P::FormalPowerSeries{T}) = length(P) > 1 && P.c[1]==0 && P.c[2] != 0
+function isalmostunit{T}(P::FormalPowerSeries{T})
+    (has(P.c, 0) && P.c[0]!=0) ? (return false) : true
+    (has(P.c, 1) && P.c[1]!=0) ? (return true) : (return false)
+end
+
+###############################
+# Formal Laurent series (fLs) #
+###############################
+
+type FormalLaurentSeries{Coefficients}
+    c :: Dict{BigInt, Coefficients}
+    FormalLaurentSeries{Coefficients}(c::Dict{BigInt, Coefficients}) = new(c)
+    function FormalLaurentSeries{Coefficients}(v::Vector{Coefficients})
+       c = new(c)
+       for i=1:length(v)
+           c[i] = v[i]
+       end
+       c
+    end
+end
+    
+
 ###########
 # Sandbox #
 ###########
 
-MaxSeriesSize=50
-MaxRange = 1000
-MatrixSize=150
+MaxSeriesSize=10
+MaxRange = 50
+MatrixSize=15
+TT=Int64
 
 (n1, n2, n3) = int(rand(3)*MaxSeriesSize)
 
-X = FormalPowerSeries{Int64}(int(rand(n1)*MaxRange))
-Y = FormalPowerSeries{Int64}(int(rand(n2)*MaxRange))
-Z = FormalPowerSeries{Int64}(int(rand(n3)*MaxRange))
+X = FormalPowerSeries{TT}(int(rand(n1)*MaxRange))
+Y = FormalPowerSeries{TT}(int(rand(n2)*MaxRange))
+Z = FormalPowerSeries{TT}(int(rand(n3)*MaxRange))
 
 c = int(rand()*MatrixSize) #Size of matrix representation to generate
 
-
-#Test very basic things
-@assert length(X) == length(X.c)
-@assert length(Y) == length(Y.c)
-
-TT = typeof(X.c[1])
 nzeros = int(rand()*MaxSeriesSize)
-@assert trim(FormalPowerSeries{TT}([X.c; zeros(TT, nzeros)])) == X
-@assert trim(FormalPowerSeries{TT}([Y.c; zeros(TT, nzeros)])) == Y
-
-#padded vectors for later testing
-Xv = [X.c; zeros(TT, max(0, length(Y.c)-length(X.c)))]
-Yv = [Y.c; zeros(TT, max(0, length(X.c)-length(Y.c)))]
+@assert X == trim(X)
+XX = deepcopy(X)
+for i=1:nzeros  
+    idx = int(rand()*MaxRange)
+    if !has(XX.c, idx)
+        XX.c[idx] = convert(TT, 0)
+    end
+end
+@assert trim(XX) == X
 
 #Test addition, p.15, (1.3-4)
-@assert (X+X).c == 2X.c
-@assert (X+Y).c == Xv + Yv
-@assert (Y+X).c == Yv + Xv
-@assert (Y+Y).c == 2Y.c
+@assert X+X == 2X
+@assert X+Y == Y+X
 @assert MatrixForm(X+Y,c) == MatrixForm(X,c)+MatrixForm(Y,c)
 
 #Test subtraction, p.15, (1.3-4)
-@assert (X-X).c == 0X.c
-@assert (X-Y).c == Xv - Yv
-@assert (Y-X).c == Yv - Xv
-@assert (Y-Y).c == 0Y.c
+@assert X-X == 0X
+@assert X-Y == -(Y-X)
 @assert MatrixForm(X-Y,c) == MatrixForm(X,c)-MatrixForm(Y,c)
 
 #Test multiplication, p.15, (1.3-5)
+@assert X*Y == Y*X
 @assert MatrixForm(X*X,c) == MatrixForm(X,c)*MatrixForm(X,c)
 @assert MatrixForm(X*Y,c) == MatrixForm(X,c)*MatrixForm(Y,c)
 @assert MatrixForm(Y*X,c) == MatrixForm(Y,c)*MatrixForm(X,c)
 @assert MatrixForm(Y*Y,c) == MatrixForm(Y,c)*MatrixForm(Y,c)
 
-@assert X.*X == Xv.*Xv
-@assert X.*Y == Xv.*Yv
-@assert Y.*X == Yv.*Xv
-@assert Y.*Y == Yv.*Yv
+@assert X.*Y == Y.*X
 
-#Test reversion of series
 #The reciprocal series has associated matrix that is the matrix inverse
 #of the original series
-#@assert inv(float(MatrixForm(X,c)))[1, :]'[:, 1] == inv(X, c)
+#Force reciprocal to exist
+X.c[0] = 1
+discrepancy = (norm(inv(float(MatrixForm(X,c)))[1, :]'[:, 1] - tovector(reciprocal(X, c),c)))
+if discrepancy > c*sqrt(eps())
+    error(sprintf("Error %f exceeds tolerance %f", discrepancy, c*sqrt(eps())))
+end
+#@assert norm(inv(float(MatrixForm(X,c)))[1, :]'[:, 1] - tovector(reciprocal(X, c),c)) < c*sqrt(eps())
 
 #Test differentiation
-XX = X
-for i =1:length(X)-1
-    XX = derivative(XX)
+XX = derivative(X)
+for (k, v) in XX.c
+    k==0 ? continue : true
+    @assert X.c[k+1] == v/(k+1)
 end
-@assert XX == [X.c[end]*factorial(length(X)-1)]
 
 #Test product rule [H, Sec.1.4, p.19]
 @assert derivative(X*Y) == derivative(X)*Y + X*derivative(Y)
-
-#Test composition against the one-liner
-compose_quickanddirty{T <: Number}(P::FormalPowerSeries{T}, Q::FormalPowerSeries{T}) = sum([P.c[i] * Q^(i-1) for i=1:length(P.c)]) 
-
-#@assert ⋅(X,Y) == compose(X, Y)
-@assert compose(X, X) == compose_quickanddirty(X, X)
-@assert compose(X, Y) == compose_quickanddirty(X, Y)
-@assert compose(Y, X) == compose_quickanddirty(Y, X)
-@assert compose(Y, Y) == compose_quickanddirty(Y, Y)
 
 #Test right distributive law of composition [H, Sec.1.6, p.38]
 @assert compose(X,Z)*compose(Y,Z) == compose(X*Y, Z)
