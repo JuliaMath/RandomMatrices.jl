16e17729fa29b0953d4f1727514311530205ee19
Jameson Nash <vtjnash@gmail.com>
2013-05-21 01:13:35 -0400

    add println code to jacobian2by2 to see output

--- demos/book/9/jacobian2by2.jl
+++ demos/book/9/jacobian2by2.jl
@@ -10,26 +10,29 @@ warn("This may use features of PyCall and/or SymPy which are not publicly availa
 using PyCall
 pyinitialize("/opt/local/bin/python2")
 using SymPy
+jacobian(Y,X) = Sym(Y[:reshape](1,length(Y))[:jacobian](X[:reshape](1,length(X))))
 
 p,q,r,s, a,b,c,d, t, e1,e2 = Sym(:p,:q,:r,:s, :a,:b,:c,:d, :t, :e1,:e2)
 X = SymMatrix([p q; r s])
 A = SymMatrix([a b; c d])
-jacobian(Y,X) = Sym(Y[:reshape](1,length(Y))[:jacobian](X[:reshape](1,length(X))))
-Y = X^2; J = jacobian(Y,X); JAC_square = Sym(J[:det]()[:factor]())
-Y = X^3; J = jacobian(Y,X); JAC_cube = Sym(J[:det]()[:factor]())
-Y = X[:inv](); J = jacobian(Y,X); JAC_cube = Sym(J[:det]()[:factor]())
-Y = A*X; J = jacobian(Y,X); JAC_linear = Sym(J[:det]()[:factor]())
-Y = SymMatrix([p q; r/p X[:det]()/p]); J = jacobian(Y,X); JAC_lu = Sym(J[:det]()[:factor]())
+
+Y = X^2; J = jacobian(Y,X); JAC_square = Sym(J[:det]()[:factor]()); println("square:\n", J, "\n\n", JAC_square, "\n\n")
+Y = X^3; J = jacobian(Y,X); JAC_cube = Sym(J[:det]()[:factor]()); println("cube:\n", J, "\n\n", JAC_cube, "\n\n")
+Y = X[:inv](); J = jacobian(Y,X); JAC_inv = Sym(J[:det]()[:factor]()); println("inv:\n", J, "\n\n", JAC_inv, "\n\n")
+Y = A*X; J = jacobian(Y,X); JAC_linear = Sym(J[:det]()[:factor]()); println("linear:\n", J, "\n\n", JAC_linear, "\n\n")
+Y = SymMatrix([p q; r/p X[:det]()/p]); J = jacobian(Y,X); JAC_lu = Sym(J[:det]()[:factor]()); println("lu:\n", J, "\n\n", JAC_lu, "\n\n")
 
 x = SymMatrix([p, s, r])
 y = SymMatrix([sqrt(p), sqrt(s), r/(sqrt(p)*sqrt(s))])
 J = jacobian(y,x)
 JAC_DMD = Sym(J[:det]()[:factor]())
+println("DMD:\n", J, "\n\n", JAC_DMD, "\n\n")
 
 x = SymMatrix([p, s])
 y = SymMatrix([sqrt(p^2 + s^2), atan(s/p)])
 J = jacobian(y,x)
 JAC_notrace = Sym(J[:det]()[:factor]())
+println("notrace:\n", J, "\n\n", JAC_notrace, "\n\n")
 
 Q = SymMatrix([cos(t)  -sin(t); sin(t)  cos(t)])
 D = SymMatrix([e1 0; 0 e2])
@@ -38,6 +41,7 @@ y = SymMatrix([Y[1,1], Y[2,2], Y[1,2]])
 x = SymMatrix([t, e1, e2])
 J = jacobian(Y,X)
 JAC_symeig = Sym(J[:det]()[:factor]())
+println("symeig:\n", J, "\n\n", JAC_symeig, "\n\n")
 
 X = SymMatrix([p s; s r])
 Y = Sym(A[:transpose]())*X*A
@@ -45,4 +49,5 @@ y = SymMatrix([Y[1,1], Y[2,2], Y[1,2]])
 x = SymMatrix([p, r, s])
 J = jacobian(Y,X)
 JAC_symcong = Sym(J[:det]()[:factor]())
+println("symcong:\n", J, "\n\n", JAC_symcong, "\n\n")
 
