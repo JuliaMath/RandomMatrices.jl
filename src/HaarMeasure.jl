7de98fb3ca69646676791f05053637bbff31b9f6
Jiahao Chen <jiahao@mit.edu>
2013-10-17 22:04:25 -0400

    Factored up Ginibre random matrix ensembles

--- src/HaarMeasure.jl
+++ src/HaarMeasure.jl
@@ -1,5 +1,4 @@
-export rand, Ginibre, NeedPiecewiseCorrection, jpdfGinibre,
-    CircularOrthogonal, CircularUnitary, CircularSymplectic
+export rand, NeedPiecewiseCorrection
 import Base.rand
 
 # Computes samplex of real or complex Haar matrices of size nxn
@@ -65,57 +64,6 @@ function NeedPiecewiseCorrection()
     return any([x<0 for x in diag(R)])
 end
 
-
-#Samples a matrix from the Ginibre ensemble
-#This ensemble lives in GL(N, F), the set of all invertible N x N matrices
-#over the field F
-#For beta=1,2,4, F=R, C, H respectively
-immutable Ginibre <: ContinuousMatrixDistribution
-   beta::Float64
-   N::Integer
-end
-
-function rand(W::Ginibre)
-    if beta==1
-        randn(n,n)
-    elseif beta==2
-        randn(n,n)+im*randn(n,n)
-    elseif beta==4
-        Q0=randn(n,n)
-        Q1=randn(n,n)
-        Q2=randn(n,n)
-        Q3=randn(n,n)
-        [Q0+im*Q1 Q2+im*Q3;-Q2+im*Q3 Q0-im*Q1]
-    else 
-        error(string("beta = ", beta, " not implemented"))
-    end
-end
-
-function jpdfGinibre{z<:Complex}(Z::AbstractMatrix{z})
-    pi^(size(Z,1)^2)*exp(-trace(Z'*Z))
-end
-
-#Dyson Circular orthogonal, unitary and symplectic ensembles
-immutable CircularOrthogonal
-    N :: Int64
-end
-
-function rand(M::CircularOrthogonal)
-    U = rand(Ginibre(2, M.N))
-    U * U'
-end
-
-immutable CircularUnitary
-    N :: Int64
-end
-
-rand(M::CircularUnitary) = rand(Ginibre(2, M.N))
-
-immutable CircularSymplectic
-    N :: Int64
-end
-
-rand(M::CircularSymplectic) = error("Not implemented")
 
 #TODO maybe, someday
 #Haar measure on U(N) in terms of local coordinates
