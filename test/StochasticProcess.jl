ff98613429c8de3d41e7477ba92e433586388523
Jiahao Chen <jiahao@mit.edu>
2016-05-07 21:22:19 -0400

    Fix stupid bug in test/StochasticProcess

--- test/StochasticProcess.jl
+++ test/StochasticProcess.jl
@@ -4,19 +4,24 @@ using Base.Test
 srand(1)
 dx = 0.001
 
+let
 p = WhiteNoiseProcess(dx)
 @test start(p) == nothing
-next(p)
+S = next(p)
 @test done(p, S) == false
+end
 
+let
 p = BrownianProcess(dx)
 @test start(p) == 0
-next(p, 0.0)
+S = next(p, 0.0)
 @test done(p, S) == false
+end
 
+let
 p = AiryProcess(dx, 2.0)
 S = start(p)
 @test S == fill(-2/dx^2, 1, 1)
-next(p, S)
+S = next(p, S)
 @test done(p, S) == false
-
+end
