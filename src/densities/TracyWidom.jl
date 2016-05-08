bf829e0ac2f7351a24f8b9719e61a354c7fbe2cb
Jiahao Chen <jiahao@mit.edu>
2016-05-07 22:56:08 -0400

    Fix up documentation for Tracy-Widom

--- src/densities/TracyWidom.jl
+++ src/densities/TracyWidom.jl
@@ -2,12 +2,28 @@ export TracyWidom
 
 include("gradient.jl")
 
+"""
+Tracy-Widom distribution
+
+The probability distribution of the normalized largest eigenvalue of a random
+Hermitian matrix.
+
+References:
+
+1. doi:10.1016/0370-2693(93)91114-3
+2. doi:10.1007/BF02100489
+
+Numerical routines adapted from Alan Edelman's course notes for MIT 18.338,
+Random Matrix Theory, 2016.
+"""
 immutable TracyWidom <: ContinuousUnivariateDistribution end
 
 
 """
+Probability density function of the Tracy-Widom distribution
+
 Computes the Tracy-Widom distribution by directly solving the
-Painlevé II equation by numerical integration
+Painlevé II equation using the ode23 numerical integrator
 """
 function pdf{S<:Real}(d::TracyWidom, t::S, t0::S = convert(S, -8.0))
     t≤t0 && return 0.0
@@ -21,8 +37,10 @@ end
 pdf(d::Type{TracyWidom}, t::Real) = pdf(d(), t)
 
 """
+Cumulative density function of the Tracy-Widom distribution
+
 Computes the Tracy-Widom distribution by directly solving the
-Painlevé II equation by numerical integration
+Painlevé II equation using the ode23 numerical integrator
 """
 function cdf{S<:Real}(d::TracyWidom, t::S, t0::S = convert(S, -8.0))
     t≤t0 && return 0.0
@@ -34,6 +52,8 @@ function cdf{S<:Real}(d::TracyWidom, t::S, t0::S = convert(S, -8.0))
 end
 cdf(d::Type{TracyWidom}, t::Real) = cdf(d(), t)
 
+# An internal function which sets up the Painleve II differential equation and
+# runs it through the ode23 numerical integrator
 function _solve_painleve_ii{S<:Real}(t0::S, t::S)
     deq(t, y) = [y[2], t*y[1]+2y[1]^3, y[4], y[1]^2]
     a0 = airy(t0)
@@ -43,7 +63,7 @@ function _solve_painleve_ii{S<:Real}(t0::S, t::S)
 end
 
 """
-Samples the largest eigenvalue of the n x n GUE matrix
+Samples the largest eigenvalue of the n × n GUE matrix
 """
 function rand(d::TracyWidom, n::Integer)
     n > 1 || error("Cannot construct $n × $n matrix")
