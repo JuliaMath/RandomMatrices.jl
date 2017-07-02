a9a89f37bb99f2610754ddd9e66fd6e07448b845
Christopher Rackauckas <accounts@chrisrackauckas.com>
2017-07-02 06:38:27 -0700

    add `using DiffEqBase` for v0.5

--- src/RandomMatrices.jl
+++ src/RandomMatrices.jl
@@ -4,6 +4,7 @@ using Combinatorics
 using Compat
 using GSL
 using OrdinaryDiffEq
+using DiffEqBase # This line is only needed on v0.5, and comes free from OrdinaryDiffEq on v0.6
 
 import Base: isinf, rand
 import Distributions: ContinuousUnivariateDistribution,
